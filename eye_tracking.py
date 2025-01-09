53073d3aeecc2e681025d5a2de5476247a0f1fd5
pierre_hurlin <166821586+peuhier@users.noreply.github.com>
2025-01-09 15:07:59 +0100

    Both eyes are detected now

--- eye_tracking.py
+++ eye_tracking.py
@@ -5,7 +5,7 @@ import dlib
 cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+predictor = dlib.shape_predictor('C:/Users/hurli/Documents/Eye_tracking/shape_predictor_68_face_landmarks.dat')
 
 def midpoint(p1,p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
@@ -22,14 +22,20 @@ while True:
         #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
         landmarks = predictor(gray, face)
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
-        top_point = midpoint(landmarks.part(37),landmarks.part(38))
-        bottom_point = midpoint(landmarks.part(41),landmarks.part(40))
+        left_point_R = (landmarks.part(36).x, landmarks.part(36).y)
+        right_point_R = (landmarks.part(39).x, landmarks.part(39).y)
+        top_point_R = midpoint(landmarks.part(37),landmarks.part(38))
+        bottom_point_R = midpoint(landmarks.part(41),landmarks.part(40))
 
-        hor_line = cv2.line(frame, left_point,right_point,(0,255,0),2)
-        ver_line = cv2.line(frame, top_point,bottom_point,(0,255,0),2)
+        left_point_L = (landmarks.part(42).x, landmarks.part(42).y)
+        right_point_L = (landmarks.part(45).x, landmarks.part(45).y)
+        top_point_L = midpoint(landmarks.part(43),landmarks.part(44))
+        bottom_point_L = midpoint(landmarks.part(46),landmarks.part(47))
 
+        hor_line_R = cv2.line(frame, left_point_R,right_point_R,(0,255,0),2)
+        ver_line_R = cv2.line(frame, top_point_R,bottom_point_R,(0,255,0),2)
+        hor_line_L = cv2.line(frame, left_point_L,right_point_L,(0,255,0),2)
+        ver_line_L = cv2.line(frame, top_point_L,bottom_point_L,(0,255,0),2)
 
     cv2.imshow('frame', frame)
 
