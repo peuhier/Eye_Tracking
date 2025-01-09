df7460c72536151b19f9a493e988e2e14a9afd95
pierre_hurlin <166821586+peuhier@users.noreply.github.com>
2025-01-09 11:15:07 +0100

    Added horizonal and vertical lines in the eye

--- eye_tracking.py
+++ eye_tracking.py
@@ -7,6 +7,9 @@ cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
+def midpoint(p1,p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -19,9 +22,14 @@ while True:
         #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
         landmarks = predictor(gray, face)
-        x = landmarks.part(36).x
-        y = landmarks.part(36).y
-        cv2.circle(frame, (x, y), 2, (0, 0, 255), 2)
+        left_point = (landmarks.part(36).x, landmarks.part(36).y)
+        right_point = (landmarks.part(39).x, landmarks.part(39).y)
+        top_point = midpoint(landmarks.part(37),landmarks.part(38))
+        bottom_point = midpoint(landmarks.part(41),landmarks.part(40))
+
+        hor_line = cv2.line(frame, left_point,right_point,(0,255,0),2)
+        ver_line = cv2.line(frame, top_point,bottom_point,(0,255,0),2)
+
 
     cv2.imshow('frame', frame)
 
