af765beb42fdd633a139ebb6ec1564e01dbf25b6
pierre_hurlin <166821586+peuhier@users.noreply.github.com>
2025-01-09 11:02:19 +0100

    Added face recognition

--- eye_tracking.py
+++ eye_tracking.py
@@ -1,10 +1,22 @@
 import cv2
 import numpy as np
 import dlib
+
 cap = cv2.VideoCapture(0)
+
+detector = dlib.get_frontal_face_detector()
 
 while True:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+
+    for face in faces:
+        x1, y1 = face.left(),face.top()
+        x2 ,y2 = face.right(), face.bottom()
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
     cv2.imshow('frame', frame)
 
     key = cv2.waitKey(1)
