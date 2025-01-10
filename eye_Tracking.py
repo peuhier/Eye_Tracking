2d7e99d678cee42c77fa5c2ce6489fcbfae20274
pierre_hurlin <166821586+peuhier@users.noreply.github.com>
2025-01-10 15:40:11 +0100

    pupil tracking done

--- eye_Tracking.py
+++ eye_Tracking.py
@@ -11,7 +11,9 @@ def midpoint(p1,p2):
 while True:
 
     ret, frame = cap.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if ret is False:
+        break
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(frame)
 
@@ -35,18 +37,23 @@ while True:
         max_y = max(left_eye_region[:,1])
 
         eye_frame = frame[min_y:max_y, min_x:max_x]
-
-
-        eye_frame = cv2.resize(eye_frame, (0, 0), fx=5, fy=5)
+        eye_color = cv2.resize(eye_frame, (200,100))
+        eye_frame = cv2.cvtColor(eye_color, cv2.COLOR_BGR2GRAY)
+        rows, cols = eye_frame.shape
         eye_frame = cv2.GaussianBlur(eye_frame,(7,7),0)
         _,threshold = cv2.threshold(eye_frame,70,255,cv2.THRESH_BINARY_INV)
         contours,_ = cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        
+        contours = sorted(contours,key=lambda x:cv2.contourArea(x),reverse=True)
         for cnt in contours:
-            cv2.drawContours(eye_frame,[cnt],-1,(0,0,255),3)
+            x,y,w,h = cv2.boundingRect(cnt)
+            #cv2.drawContours(eye_frame,[cnt],-1,(0,0,255),3)
+            cv2.rectangle(eye_color,(x,y),(x+w,y+h),(255,0,0),1)
+            cv2.line(eye_color,(x+int(w/2),0),(x+int(w/2),rows),(0,255,0),1)
+            cv2.line(eye_color,(0,y+int(h/2)),(cols,y+int(h/2)),(0,255,0),1)
+            break
         cv2.imshow('threshold',threshold)
-        cv2.imshow('eye',eye_frame)
-    cv2.imshow('frame', frame)
+        cv2.imshow('eye',eye_color)
+    #cv2.imshow('frame', frame)
 
     key = cv2.waitKey(30)
     if key == 27:
